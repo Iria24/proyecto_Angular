1187229a20761784e090852aeb895ca5a64fd36d
iagolpriv <iagolpriv@gmail.com>
2023-11-09 18:27:22 +0100

    estructurado ok funcionalidades productos corregido

--- StoreAngular/src/app/productos/producto-list/producto-list.component.ts
+++ StoreAngular/src/app/productos/producto-list/producto-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from './../product-service.service';
 import { IProduct } from './../i-product'; // Importa la interfaz de producto
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-producto-list',
@@ -11,7 +12,7 @@ export class ProductoListComponent implements OnInit {
   
   productos: IProduct[] = [];
 
-  constructor(private productService: ProductService) {}
+  constructor(private productService: ProductService, private router: Router) {}
 
   ngOnInit(): void {  //aqui obtiene los datos de la api
     this.productService.findAll().subscribe(data => {
@@ -22,6 +23,12 @@ export class ProductoListComponent implements OnInit {
   handleButtonClick(product: IProduct): void {
     // Aquí puedes manejar el clic en el botón.
     // Por ejemplo, podrías navegar a la página de detalles del producto.
-    console.log(product);
+    this.router.navigate(['/productos', product.id]);
+  }
+  
+  editButtonClick(product: IProduct): void {
+      // Aquí puedes manejar el clic en el botón.
+      // Por ejemplo, podrías navegar a la página de detalles del producto.
+    this.router.navigate(['/productos/edit', product.id]);
   }
 }
--- StoreAngular/src/app/user/user.module.ts
+++ StoreAngular/src/app/user/user.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserDetailComponent } from './user-detail/user-detail.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from '../login/login.component';
+import { RegisterComponent } from '../register/register.component';
 
 
 
